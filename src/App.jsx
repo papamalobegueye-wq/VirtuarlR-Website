153cec57607be441ff96d4e6e860b59d29fb307c
maal <papamalobegueye-wq>
2026-07-05 02:41:11 +0000

    fin Projet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 // import React from 'react'
 
+import FeatureSection from "./components/FeatureSection"
+import Footer from "./components/Footer"
 import HeroSection from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Pricing from "./components/Pricing"
+import Testimonials from "./components/Testimonials"
+import Workflow from "./components/Workflow"
 
 const App = () => {
   return (
@@ -9,7 +14,12 @@ const App = () => {
       <Navbar />
 
       <div className="max-w-7xl mx-auto pt-20 px-6" >
-        <HeroSection/>
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
       </div>
     </div>
   )
